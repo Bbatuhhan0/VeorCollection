7c778757ce52e75c87c314e738b643a61599376d
batuh <batuh@DESKTOP-DK1RG2G>
2025-11-07 21:38:48 +0300

    Proje dosyası ekle.

---
+++ VeorCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VeorCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
