db6276122c86c227742873bce2e34bdc62033178
batuh <batuh@DESKTOP-DK1RG2G>
2025-12-08 00:30:35 +0300

    ürün sergileme

--- VeorCollection/Controllers/HomeController.cs
+++ VeorCollection/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; // Bu kütüphaneyi eklemeyi unutma
+using System.Diagnostics;
+using System.Linq;
+using VeorCollection.Data;
 using VeorCollection.Models;
 
 namespace VeorCollection.Controllers
@@ -7,15 +10,25 @@ namespace VeorCollection.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context; // Veritabaný baðlantýsý
 
-        public HomeController(ILogger<HomeController> logger)
+        // Constructor'a context'i ekliyoruz
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Veritabanýndan en son eklenen 8 ürünü çek (Id'si en büyük olanlar en yenidir)
+            var recentProducts = _context.Products
+                                         .OrderByDescending(p => p.Id)
+                                         .Take(8)
+                                         .ToList();
+
+            // Ürünleri View'a (sayfaya) gönderiyoruz
+            return View(recentProducts);
         }
 
         public IActionResult Privacy()
